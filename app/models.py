8339e74af1649aed646ac117c7fb969e780ff0bd
Orozbek28 <gulmuradovorozbek297@gmail.com>
2023-04-17 12:10:07 +0600

    added Category, item m

--- app/models.py
+++ app/models.py
@@ -3,25 +3,19 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.username
+class Category(models.Model):
+    name = models.CharField(max_length=20)
 
 
 
 
-class Comment(models.Model):
-    text = models.CharField(max_length=255)
-    date_create = models.DateField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
-    def __str__(self):
-        return self.text[:30]
+
 
 
 
